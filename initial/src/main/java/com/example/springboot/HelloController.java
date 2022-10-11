93c3758c85242b499a91c716c03d69fb19468a55
fbehrens51 <fbehrens@vmware.com>
2022-10-11 09:18:32 -0400

    updated to deploy to test platforms

--- initial/src/main/java/com/example/springboot/HelloController.java
+++ initial/src/main/java/com/example/springboot/HelloController.java
@@ -1,7 +1,13 @@
 package com.example.springboot;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.cert.Certificate;
+import java.util.Map;
 
 @RestController
 public class HelloController {
@@ -9,6 +15,17 @@ public class HelloController {
 	@GetMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot!";
+	}
+
+	@GetMapping("/listHeaders")
+	public ResponseEntity<String> listAllHeaders(
+			@RequestHeader Map<String, String> headers) {
+		headers.forEach((key, value) -> {
+			System.out.println(String.format("Header '%s' = %s", key, value));
+		});
+
+		return new ResponseEntity<String>(
+				String.format("Listed %d headers", headers.size()), HttpStatus.OK);
 	}
 
 }
